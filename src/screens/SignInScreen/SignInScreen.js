3256ef7b2c9726b8fb3fcda2f5ea571e0f7f12b3
NAVIXXX <jose.aimituma@tecsup.edu.pe>
2022-03-23 08:50:20 -0500

    first commit login

---
+++ src/screens/SignInScreen/SignInScreen.js
@@ -0,0 +1,106 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  useWindowDimensions,
+  ScrollView,
+} from 'react-native';
+import Logo from '../../../assets/images/Logo_1.png';
+import CustomInput from '../../components/CustomInput';
+import CustomButton from '../../components/CustomButton';
+import SocialSignInButtons from '../../components/SocialSignInButtons';
+import {useNavigation} from '@react-navigation/native';
+import { Auth } from 'aws-amplify';
+
+const SignInScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const {height} = useWindowDimensions();
+  const navigation = useNavigation();
+
+  const [loading, setLoading] = useState(false);
+
+  const onSignInPressed = async data => {
+    if (loading){
+      return;
+    }
+    setLoading(true);
+  
+    try {
+    const response = await Auth.signIn(data.username, data.password);
+    console.log(response);
+    }catch(e){
+      Alert.alert('Oops', e.message);
+    }
+    setLoading(false);
+    // validate user
+    // navigation.navigate('Home');
+  };
+
+  const onForgotPasswordPressed = () => {
+    navigation.navigate('ForgotPassword');
+  };
+
+  const onSignUpPress = () => {
+    navigation.navigate('SignUp');
+  };
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.root}>
+        <Image
+          source={Logo}
+          style={[styles.logo, {height: height * 0.3}]}
+          resizeMode="contain"
+        />
+
+        <CustomInput
+          placeholder="Username"
+          value={username}
+          setValue={setUsername}
+        />
+        <CustomInput
+          placeholder="Password"
+          value={password}
+          setValue={setPassword}
+          secureTextEntry
+        />
+
+        <CustomButton
+         text={loading ? "Loading..." : "Sign In"}
+         onPress = { handleSubmit (onSignInPressed)} />
+
+        <CustomButton
+          text="Forgot password?"
+          onPress={onForgotPasswordPressed}
+          type="TERTIARY"
+        />
+
+        <SocialSignInButtons />
+
+        <CustomButton
+          text="Don't have an account? Create one"
+          onPress={onSignUpPress}
+          type="TERTIARY"
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: '70%',
+    maxWidth: 300,
+    maxHeight: 200,
+  },
+});
+
+export default SignInScreen;
